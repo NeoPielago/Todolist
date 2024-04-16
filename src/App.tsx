226f5768415294e1cb245d068161d9a7097d10b3
Neo <neopielago123@gmail.com>
2024-04-17 03:07:08 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <span className="heading">Todo List</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
