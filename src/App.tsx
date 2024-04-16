3247e0a3c55f1a8f497d487eb6dac901d27a22e1
Neo <neopielago123@gmail.com>
2024-04-17 04:37:14 +0800

    Add UI fix and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import InputField from "./components/InputField";
 
 function App() {
+  const [todo, setTodo] = useState<string>("");
+
+  console.log(todo);
   return (
     <>
       <div className="app">
         <span className="heading">Todo List</span>
+        <InputField todo={todo} setTodo={setTodo}></InputField>
       </div>
     </>
   );
